fb718797a6c419e5b3448db131ba13984bae932c
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-07-30 14:32:58 +0200

    improved CSS

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-
+let input = document.getElementById("input")
 
 document.getElementById("convert-btn").addEventListener("click", function(){
     variablesForEachGroup("length-conversion","meters", "feet", 3.281)
     variablesForEachGroup("volume-conversion","liters","gallons", 0.264)
     variablesForEachGroup("mass-conversion","kilos","pounds", 2.204)
+    input.value = ""
 })
 
 function variablesForEachGroup(dom, metricUnit, imperialUnit, rate){
-    let inputValue = Number(document.getElementById("input").value)
-    document.getElementById(dom).innerText = `${inputValue} ${metricUnit}  = ${(inputValue*rate).toFixed(2)} ${imperialUnit} | ${inputValue} ${imperialUnit} = ${(inputValue/rate).toFixed(2)} ${metricUnit}`
+    let inputValue = Number(input.value)
+    document.getElementById(dom).innerText = `${inputValue} ${metricUnit}  = ${(inputValue*rate).toFixed(2)} ${imperialUnit} | ${inputValue} ${imperialUnit} = ${(inputValue/rate).toFixed(2)} ${metricUnit}`    
 }
